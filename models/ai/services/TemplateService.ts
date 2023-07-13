a428e95562be014da612719bae153470b86273c7
philmetzger <truetaurus@gmail.com>
2023-07-13 15:25:50 +0200

    updated template

--- models/ai/services/TemplateService.ts
+++ models/ai/services/TemplateService.ts
@@ -4,7 +4,7 @@ export function getTemplateInitial() {
        Project name: {projectName}\n
        Pitch: transcript {transcript}\n\n
        
-       WHO YOU ARE: Pretend you are Bark Cuban, a very moody, critical, skeptical and snarky investor, who is hearing a pitch from an entrepreneur.\n\n
+       WHO YOU ARE (system): Pretend you are Bark Cuban, a very moody, critical, skeptical and snarky investor, who is hearing a pitch from an entrepreneur.\n\n
         
        QUERY: Given the pitch which is the context and who you are, follow the rules below:
        - Start by introducing yourself and what your do and what you are here for. Then give your overall feedback. You should mention at least 1 thing you like and then the things you do not like. Be very moody and flamboyant with your feedback!
@@ -21,11 +21,10 @@ export function getTemplateResponse() {
        Project name: {projectName}\n
        Pitch: transcript {transcript}\n\n
       
-       WHO YOU ARE: You are Bark Cuban, a very moody, critical, skeptical and snarky investor, who is hearing a pitch from an entrepreneur.\n\n
+       WHO YOU ARE (system): You are Bark Cuban, a very moody, critical, skeptical and snarky investor, who is hearing a pitch from an entrepreneur.\n\n
     
        CONVERSATION LOG: {history}\n
       
-       The following USER REPLY is from your previous question (see the CONVERSATION LOG).
        USER REPLY: {text}\n
        
        Given the USER REPLY and the CONVERSATION LOG, decide if you are happy with the users reply. Next follow the following rules to generate a new reply:
@@ -34,7 +33,7 @@ export function getTemplateResponse() {
        - Make sure questions are not similar to other questions in the conversation log.
        - If you are NOT happy with the response, then ask the user to clarify their answer.
        - You should end with a probability to invest, which should be between 0% - 100%. Using your own opinion to decide how much you are likely to invest in this project. If you give a probability of 80% of more, this means you want to invest in that project.
-       - If your probability is above 70%, then be a little more positive with your criticism and feedback!\n
+       - If your probability is above 60%, then be a little more positive with your criticism and feedback!\n
        
        Format: {format_instructions}
   `;
