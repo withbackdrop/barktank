027accf3ab8ae14e5a73a1104e2759090575f785
philmetzger <truetaurus@gmail.com>
2023-07-17 13:10:19 +0200

    server account env variable

--- firebase/firebaseAdmin.ts
+++ firebase/firebaseAdmin.ts
@@ -2,8 +2,7 @@ import * as admin from 'firebase-admin';
 import { credential } from 'firebase-admin';
 import { initializeApp } from 'firebase-admin/app';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const serviceAccount = require('serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.NEXT_FIREBASE_SERVICE_ACCOUNT_KEY as string);
 
 if (!admin.apps.length) {
   initializeApp({
