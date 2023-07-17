5a7726b517a5637ed33dce4c8c013cafe59aa9cf
philmetzger <truetaurus@gmail.com>
2023-07-17 13:03:37 +0200

    color

--- modules/projects/components/PitchFlow/steps/pitch/ConversationItemSystem.tsx
+++ modules/projects/components/PitchFlow/steps/pitch/ConversationItemSystem.tsx
@@ -1,12 +1,28 @@
 import { Text, Note } from '@/modules/application/components/DesignSystem';
 import SlowText from '@/modules/common/components/SlowText';
 
+const getColor = (probability: number) => {
+  if (probability < 50) {
+    return 'red';
+  }
+
+  if (probability >= 50 && probability < 60) {
+    return 'yellow';
+  }
+
+  if (probability >= 60 && probability < 80) {
+    return 'blue';
+  }
+
+  return 'green';
+};
+
 const ConversationItemSystem = ({ text, probability }) => (
   <Text tag="div">
     <strong>Bark: </strong>
     <SlowText speed={10} text={text} />
     <br />
-    <Note color="blue">
+    <Note color={getColor(probability)}>
       <strong>Probability to invest: </strong> {probability}%
     </Note>
   </Text>
