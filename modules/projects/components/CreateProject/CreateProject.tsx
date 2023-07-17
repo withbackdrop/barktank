ac1baeb5d1ae8580a04223a01617fc30d5620dea
philmetzger <truetaurus@gmail.com>
2023-07-17 17:17:50 +0200

    back

--- modules/projects/components/CreateProject/CreateProject.tsx
+++ modules/projects/components/CreateProject/CreateProject.tsx
@@ -10,7 +10,7 @@ const CreateProject = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   return (
     <Card>
-      <div className="m-auto flex h-full flex-col justify-center">
+      <div className="m-auto flex h-full flex-col items-center justify-center">
         <Heading level={1} size="xl" spacing="m" textAlign="center">
           Create new project pitch
         </Heading>
