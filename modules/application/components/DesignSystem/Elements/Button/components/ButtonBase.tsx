19604e1be1ee73c239dba3f323c6580a95270516
philmetzger <truetaurus@gmail.com>
2023-07-17 13:17:23 +0200

    button fixes

--- modules/application/components/DesignSystem/Elements/Button/components/ButtonBase.tsx
+++ modules/application/components/DesignSystem/Elements/Button/components/ButtonBase.tsx
@@ -11,37 +11,30 @@ const mapThemeBorder = {
 const mapThemeColor = {
   solid: {
     default: `text-white bg-blue-500 disabled:bg-gray-150 disabled:text-gray-400
-    hover:bg-blue-500 active:bg-black
-    focus:bg-neutral-900 focus:shadow-outline-primary
-    dark:text-gray-900 dark:bg-white dark:disabled:bg-zinc-700 dark:hover:bg-gray-200 
-    dark:active:bg-gray-200 dark:focus:bg-gray-200`,
+    hover:bg-blue-500 active:bg-blue-700
+    focus:bg-blue-700 focus:shadow-outline-primary`,
     red: `text-white bg-red-500 disabled:bg-red-400
     hover:bg-red-600 active:bg-red-600
-    focus:bg-red-600 focus:shadow-outline-danger
-    dark:bg-red-900 dark:text-red-100`,
+    focus:bg-red-600 focus:shadow-outline-danger`,
     green: `text-white bg-green-500 disabled:bg-green-500
     hover:bg-green-600 active:bg-green-600
-    focus:bg-green-600 focus:shadow-outline-danger dark:bg-lime-900 dark:hover:bg-lime-800 dark:text-lime-500 dark:disabled:bg-lime-800`,
+    focus:bg-green-600 focus:shadow-outline-danger`,
     white: 'bg-white focus:bg-gray-200 focus:shadow-outline',
-    gray: 'bg-gray-150 hover:bg-gray-300 disabled:bg-gray-300/80 disabled:text-gray-400 dark:bg-zinc-700/50 dark:focus:bg-zinc-700 dark:hover:bg-zinc-700 dark:text-white focus:shadow-outline disabled:dark:bg-zinc-800 disabled:dark:text-gray-600',
+    gray: 'bg-gray-150 hover:bg-gray-300 disabled:bg-gray-300/80 disabled:text-gray-400 focus:shadow-outline',
   },
   ghost: {
-    default: `text-gray-900 border-gray-900 disabled:text-gray-600 disabled:border-gray-300
-    dark:text-gray-100 dark:border-zinc-100 
-    dark:disabled:text-gray-600
-    dark:disabled:border-zinc-700`,
-    red: `hover:text-red-400 text-red-500 border-red-500
-    hover:border-red-400 focus:shadow-outline-danger`,
-    green: `text-green-500 border-green-500 hover:border-green-600 focus:bg-green-500 focus:shadow-outline-danger dark:border-lime-500 dark:text-lime-500 dark:hover:border-lime-700 dark:disabled:border-lime-800 dark:disabled:text-lime-800`,
+    default: `text-blue-500 border-blue-500 disabled:text-gray-600 disabled:border-gray-300`,
+    red: `hover:text-red-400 text-red-500 border-red-500 hover:border-red-400 focus:shadow-outline-danger`,
+    green: `text-green-500 border-green-500 hover:border-green-600 focus:bg-green-500 focus:shadow-outline-danger`,
     white: 'text-white border-white hover:text-gray-900 hover:bg-white focus:shadow-outline',
-    gray: `text-gray-700 border-gray-300 dark:text-gray-300 dark:border-zinc-400 dark:hover:text-gray-300 dark:hover:border-zinc-200 active:bg-zinc-600 hover:border-gray-500`,
+    gray: `text-gray-700 border-gray-300 active:bg-zinc-600 hover:border-gray-500`,
   },
   bare: {
-    default: 'text-gray-800 hover:text-black dark:text-gray-100 hover:dark:text-gray-50',
+    default: 'text-blue-500 hover:text-blue-700',
     red: 'text-red-500',
     green: 'text-green-500',
     white: 'text-white',
-    gray: 'text-gray-600 hover:text-gray-800 dark:text-gray-200 dark:hover:text-gray-50',
+    gray: 'text-gray-600 hover:text-gray-800',
   },
 };
 
