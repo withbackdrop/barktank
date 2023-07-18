9007420da3b4a81f9b9198f047a803e751b8c526
Rapha Menezes <caixetamenezes@gmail.com>
2023-07-18 18:15:27 +0200

    more

--- modules/projects/components/ProjectsList/ProjectListItem.tsx
+++ modules/projects/components/ProjectsList/ProjectListItem.tsx
@@ -2,11 +2,11 @@
 
 import { getUrlProject } from '@/models/application/services/UrlService';
 import { ProjectInterface } from '@/models/projects/interfaces/ProjectInterface';
-import { Button, Card, Hyperlink, Text } from '@/modules/application/components/DesignSystem';
+import { Button, Card, Text } from '@/modules/application/components/DesignSystem';
 
 const ProjectListItem = ({ project }: { project: ProjectInterface }) => (
   <Card>
-    <div className="flex flex-col grow">
+    <div className="flex grow flex-col">
       <Text size="l" fontWeight="bold" spacing="m">
         {project.name}
       </Text>
